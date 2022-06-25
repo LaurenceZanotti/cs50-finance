e84276ae9e07db7d5da0e8925c0f0ce210422e60
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-06-25 17:46:41 -0300

    Presentation changes for master branch

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CS50 Finance
 
-CS50 Finance allows you to quote, buy and sell stocks. You can also view the stock prices in real time and the account history. More information about the project can be at [CS50 specifications](https://cs50.harvard.edu/x/2022/psets/9/finance/).
+CS50 Finance allows you to quote, buy and sell stocks. Start with $10,000 and try to earn profit! (It's a simulation of course) You can also view the stock prices in real time and the account history.
+
+More information about the project can be found at [CS50 specifications](https://cs50.harvard.edu/x/2022/psets/9/finance/).
+
+See the project live at https://pacific-dawn-05944.herokuapp.com/
 
 This project is my submission for the Problem Set 9 of CS50 Introduction to Computer Science course from Harvard, which I took at 2020.
 
--- application.py
+++ application.py
@@ -28,19 +28,19 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///finance.db")
 
 # Configure CS50 Library to use Postgres database
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
-db = SQL(uri)
+# uri = os.getenv("DATABASE_URL")
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://")
+# db = SQL(uri)
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
